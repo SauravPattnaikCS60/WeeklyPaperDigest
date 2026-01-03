22bbc49b6d396a22195102961787cd2a66ea2fd5
Saurav Pattnaik <sauravpattnaik@Sauravs-MacBook-Pro.local>
2026-01-03 15:26:44 +0530

    ui changes and tracker functionality for all papers

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
     paper_url_content = fetch_paper_content()
     recommendations = generate_feed(paper_url_content)
     if recommendations:
+        # save recommendations
         json.dump(recommendations,open("data/recommendations.json","w"))
+        
+        # update recommendations file (track all papers)
+        old_recommendations = json.load(open("data/all_recommendations.json","r"))        
+        all_recommendations = old_recommendations + recommendations
+        json.dump(all_recommendations, open("data/all_recommendations.json","w"))
 
 if __name__ == "__main__":
     main()
